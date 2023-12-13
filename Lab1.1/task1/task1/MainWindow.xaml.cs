20f04c5b0bcab0e6f387559c82e245761373b0b3
malinroot <24213684+pavel-malin@users.noreply.github.com>
2023-12-13 21:18:21 +0300

    Add files via upload

---
+++ Lab1.1/task1/task1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace task1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(NumberATextBox.Text, out double numberA) &&
+                double.TryParse(NumberBTextBox.Text, out double numberB))
+            {
+                string operation = ((Button)sender).Content.ToString();
+                double result = 0;
+
+                switch (operation)
+                {
+                    case "+":
+                        result = numberA + numberB;
+                        break;
+                    case "-":
+                        result = numberA - numberB;
+                        break;
+                    case "*":
+                        result = numberA * numberB;
+                        break;
+                    case "/":
+                        if (numberB != 0)
+                        {
+                            result = numberA / numberB;
+                        }
+                        else
+                        {
+                            ResultTextBox.Text = "Ошибка: деление на ноль!";
+                            return;
+                        }
+                        break;
+                }
+
+                ResultTextBox.Text = result.ToString();
+            }
+            else
+            {
+                ResultTextBox.Text = "Ошибка: введите числа в поля 'А' и 'Б'!";
+            }
+        }
+    }
+}
---
+++ Lab1.2/task2/task2/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace task2
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> items;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Создаем пустой список строк
+            items = new ObservableCollection<string>();
+
+            // Привязываем список к элементу ListBox
+            listBox.ItemsSource = items;
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            string text = textBox.Text;
+
+            // Добавляем текст в список
+            items.Add(text);
+
+            // Очищаем текстовое поле
+            textBox.Clear();
+        }
+    }
+}
